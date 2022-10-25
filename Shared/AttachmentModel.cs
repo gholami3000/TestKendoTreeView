96a7cab8fa33e3748d8586ee809df9e9f7aa9ed0
esmail <gholami3000@gmail.com>
2022-10-25 23:11:55 +0330

    .

---
+++ Shared/AttachmentModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class AttachmentModel
+    {
+        public string Name { get; set; }
+        public IFormCollection Files { get; set; }
+    }
+}
---
+++ TestKendoTreeView/Controllers/AttachmentController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+
+namespace TestKendoTreeView.Controllers
+{
+    public class AttachmentController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SaveFile()
+        {
+            using var client = new HttpClient();
+
+            foreach (var file in Request.Form.Files)
+            {
+                if (file.Length <= 0)
+                    continue;
+
+                var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+
+                using (var content = new MultipartFormDataContent())
+                {
+                    content.Add(new StreamContent(file.OpenReadStream())
+                    {
+                        Headers =
+                    {
+                        ContentLength = file.Length,
+                        ContentType = new MediaTypeHeaderValue(file.ContentType)
+                    }
+                    }, "File", fileName);
+
+                    var response = await client.PostAsync("http://localhost:5065/WeatherForecast/AttachFile?name=ho", content);
+                }
+            }
+            return Json("ok");
+        }
+
+    }
+}
